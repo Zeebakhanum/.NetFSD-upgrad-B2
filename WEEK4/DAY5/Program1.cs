c9b15d567d0fbc2313c1dba40c761f9fe8d5399e
zeeba khan <zeebakhanum47@gmail.com>
2026-03-15 20:28:45 +0530

    Initial commit

---
+++ WEEK4/DAY5/Program1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class BankAccount
+{
+    // Private field
+    private double balance;
+
+    // Deposit method
+    public void Deposit(double amount)
+    {
+        balance = balance + amount;
+    }
+
+    // Withdraw method
+    public void Withdraw(double amount)
+    {
+        if (amount <= balance)
+        {
+            balance = balance - amount;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient balance");
+        }
+    }
+
+    // GetBalance method
+    public double GetBalance()
+    {
+        return balance;
+    }
+}
+
+class Program1
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount();
+
+        account.Deposit(1000);
+        account.Withdraw(300);
+
+        Console.WriteLine("Current Balance = " + account.GetBalance());
+    }
+}
